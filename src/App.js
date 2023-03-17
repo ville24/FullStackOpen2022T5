a0919c154d10937201356dafe2f6bdb706766e04
ville24 <73233385+ville24@users.noreply.github.com>
2023-03-17 12:32:47 +0200

    t5.10

--- src/App.js
+++ src/App.js
@@ -155,7 +155,7 @@ const App = () => {
               <BlogForm createBlog={addBlog} />
             </Togglable>
             <div>
-              {blogs.map(blog =>
+              {blogs.sort((a, b) => b.likes - a.likes).map(blog =>
                 <Blog key={blog.id} blog={blog} updateBlog={updateBlog} />
               )}
             </div>
