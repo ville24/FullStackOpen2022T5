417ccac568266dae21487068cef71b7407b0a79e
ville24 <73233385+ville24@users.noreply.github.com>
2023-03-24 11:27:21 +0200

    t5.23 2

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -1,18 +1,18 @@
 describe('Blog app', function() {
   beforeEach(function() {
-    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    cy.request('POST', `${Cypress.env('BACKEND')}/testing/reset`)
     const user = {
       name: 'Matti Meikäläinen',
       username: 'mmeikala',
       password: 'salasana'
     }
-    cy.request('POST', 'http://localhost:3003/api/users/', user)
+    cy.request('POST', `${Cypress.env('BACKEND')}/users`, user)
 
-    cy.visit('http://localhost:3000')
+    cy.visit('')
   })
 
   it('Login form is shown', function() {
-    cy.visit('http://localhost:3000')
+    cy.visit('')
     cy.contains('Log in to application')
   })
 
@@ -36,79 +36,85 @@ describe('Blog app', function() {
 
   describe('When logged in', function() {
     beforeEach(function() {
-      cy.get('#username').type('mmeikala')
-      cy.get('#password').type('salasana')
-      cy.get('#login-button').click()
-
+      cy.login({ username: 'mmeikala', password: 'salasana' })
+    })
+    it('A blog can be created', function() {
       cy.get('#newblog').click()
       cy.get('[name=\'title\']').type('React patterns')
       cy.get('[name=\'author\']').type('Michael Chan')
       cy.get('[name=\'url\']').type('https://reactpatterns.com/')
       cy.get('#createblog').click()
-    })
 
-    it('A blog can be created', function() {
       cy.contains('React patterns')
       cy.contains('Michael Chan')
     })
 
-    it('A blog can be liked', function() {
-      cy.get('.viewbutton:first').click()
-      cy.get('.likebutton:first').click()
-      cy.get('.likes:first').contains('1')
-    })
+    describe('and a blog exists', function () {
+      beforeEach(function () {
+        cy.createBlog({
+          title: 'React patterns',
+          author: 'Michael Chan',
+          url: 'https://reactpatterns.com/'
+        })
+      })
 
-    it('A blog can be removed', function() {
-      cy.get('.viewbutton:first').click()
-      cy.get('.removebutton:first').click()
-      cy.get('.info').contains('Blog deleted')
-      cy.get('html').should('not.contain', 'React patterns')
-    })
+      it('A blog can be liked', function() {
+        cy.get('.viewbutton:first').click()
+        cy.get('.likebutton:first').click()
+        cy.get('.likes:first').contains('1')
+      })
 
-    it('A blog can be removed only by the creator', function() {
-      cy.get('.viewbutton:first').click()
-      cy.get('.removebutton:first').contains('remove')
+      it('A blog can be removed', function() {
+        cy.get('.viewbutton:first').click()
+        cy.get('.removebutton:first').click()
+        cy.get('.info').contains('Blog deleted')
+        cy.get('html').should('not.contain', 'React patterns')
+      })
 
-      cy.get('#logoutbutton').click()
+      it('A blog can be removed only by the creator', function() {
+        cy.get('.viewbutton:first').click()
+        cy.get('.removebutton:first').contains('remove')
 
-      const user = {
-        name: 'Matti Meikäläinen 2',
-        username: 'mmeikal2',
-        password: 'salasana'
-      }
-      cy.request('POST', 'http://localhost:3003/api/users/', user)
+        cy.get('#logoutbutton').click()
 
-      cy.get('#username').type('mmeikal2')
-      cy.get('#password').type('salasana')
-      cy.get('#login-button').click()
+        const user = {
+          name: 'Matti Meikäläinen 2',
+          username: 'mmeikal2',
+          password: 'salasana'
+        }
+        cy.request('POST', `${Cypress.env('BACKEND')}/users`, user)
 
-      cy.get('.viewbutton:first').click()
-      cy.get('removebutton').should('not.exist')
-    })
+        cy.login({ username: 'mmeikal2', password: 'salasana' })
 
-    it('Blogs are sorted by likes', function() {
-      cy.get('.viewbutton:first').click()
-      cy.get('.likebutton:first').click()
-      cy.get('.likes:first').contains('1')
+        cy.get('.viewbutton:first').click()
+        cy.get('removebutton').should('not.exist')
+      })
 
-      cy.get('#newblog').click()
-      cy.get('[name=\'title\']').type('Go To Statement Considered Harmful')
-      cy.get('[name=\'author\']').type('Edsger W. Dijkstra')
-      cy.get('[name=\'url\']').type('http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html')
-      cy.get('#createblog').click()
-      cy.contains('Go To Statement Considered Harmful')
+      it('Blogs are sorted by likes', function() {
+        cy.get('.viewbutton:first').click()
+        cy.get('.likebutton:first').click()
+        cy.get('.likes:first').contains('1')
 
-      cy.get('.blog').eq(0).should('contain','React patterns')
-      cy.get('.blog').eq(1).should('contain','Go To Statement Considered Harmful')
+        cy.createBlog({
+          title: 'Go To Statement Considered Harmful',
+          author: 'Edsger W. Dijkstra',
+          url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html'
+        })
 
-      cy.get('.viewbutton').eq(1).click()
-      cy.get('.likebutton').eq(1).click()
-      cy.get('.likes').eq(1).contains('1')
+        cy.get('.blog').eq(0).should('contain','React patterns')
+        cy.get('.blog').eq(1).should('contain','Go To Statement Considered Harmful')
 
-      cy.get('.likebutton').eq(1).click()
+        cy.get('.viewbutton').eq(0).click()
+        cy.get('.viewbutton').eq(1).click()
 
-      cy.get('.blog').eq(0).should('contain','Go To Statement Considered Harmful')
-      cy.get('.blog').eq(1).should('contain','React patterns')
+        cy.get('.likebutton').eq(1).click()
+        cy.get('.likes').eq(1).contains('1')
+
+        cy.get('.likebutton').eq(1).click()
+
+        cy.get('.blog').eq(0).should('contain','Go To Statement Considered Harmful')
+        cy.get('.blog').eq(1).should('contain','React patterns')
+      })
     })
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,25 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('login', ({ username, password }) => {
+  cy.request('POST', `${Cypress.env('BACKEND')}/login`, {
+    username, password
+  }).then(response => {
+    localStorage.setItem('loggedBlogappUser', JSON.stringify(response.body))
+    cy.visit('')
+  })
+})
+
+Cypress.Commands.add('createBlog', ({ title, author, url }) => {
+  cy.request({
+    url: `${Cypress.env('BACKEND')}/blogs`,
+    method: 'POST',
+    body: { title, author, url },
+    headers: {
+      'Authorization': `Bearer ${JSON.parse(localStorage.getItem('loggedBlogappUser')).token}`
+    }
+  })
+
+  cy.visit('')
+})
